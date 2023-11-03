deb02626ac756449de0179975f7d82312ce09d94
Easy2Digital <77144056+easy2digital@users.noreply.github.com>
2023-11-03 12:51:53 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bfl-sdk-python',
+    version='1.0.1',
+    author='buyfromlo',
+    author_email='info@buyfromlo.com',
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+)
+
