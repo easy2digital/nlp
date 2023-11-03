f372f7b7b321e393351203930f9d0d999d2a650c
Easy2Digital <77144056+easy2digital@users.noreply.github.com>
2023-11-03 14:05:06 +0800

    Update __init__.py

--- en_core_web_sm/__init__.py
+++ en_core_web_sm/__init__.py
@@ -3,14 +3,14 @@ from spacy.util import load_model_from_init_py, get_model_meta
 
 
 
-# __version__ = get_model_meta(Path(__file__).parent)['version']
-__version__ = get_model_meta('/en_core_web_sm/meta.json')['version']
+__version__ = get_model_meta(Path(__file__).parent)['version']
+# __version__ = get_model_meta('/en_core_web_sm/meta.json')['version']
 
 
 
-
-# def load(**overrides):
-#     return load_model_from_init_py(__file__, **overrides)
 
 def load(**overrides):
-    return load_model_from_init_py('/en_core_web_sm/meta.json', **overrides)
+    return load_model_from_init_py(__file__, **overrides)
+
+# def load(**overrides):
+#     return load_model_from_init_py('/en_core_web_sm/meta.json', **overrides)
